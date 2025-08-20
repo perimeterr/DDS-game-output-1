010e3e759703b7ace22577bdf1ac0c7810e53fe7
perimeterr <mikaela.paderna@student.ateneo.edu>
2025-08-20 12:58:37 +0800

    initialize new Godot project

---
+++ player.gd
@@ -0,0 +1,55 @@
+extends Node2D
+
+@onready var tile_map = $"../TileMap"
+@onready var sprite_2d = $Sprite2D
+@onready var ray_cast_2d = $RayCast2D
+var is_moving = false
+
+
+func _physics_process(delta):
+	if is_moving == false:
+		return
+	
+	if global_position == sprite_2d.global_position:
+		is_moving = false
+		return
+		
+	sprite_2d.global_position = sprite_2d.global_position.move_toward(global_position, 1)
+
+func _process(delta):
+	if is_moving:
+		return
+		
+	if Input.is_action_pressed("up"):
+		move(Vector2.UP)
+	elif Input.is_action_pressed("down"):
+		move(Vector2.DOWN)
+	elif Input.is_action_pressed("left"):
+		move(Vector2.LEFT)
+	elif Input.is_action_pressed("right"):
+		move(Vector2.RIGHT)
+
+func move(direction: Vector2):
+	var current_tile: Vector2i = tile_map.local_to_map(global_position)
+	
+	var target_tile: Vector2i = Vector2i(
+		current_tile.x + direction.x,
+		current_tile.y + direction.y,
+	)
+	
+	var tile_data: TileData = tile_map.get_cell_tile_data(0, target_tile)
+	
+	if tile_data.get_custom_data("walkable") == false:
+		return
+		
+	ray_cast_2d.target_position = direction * 16
+	ray_cast_2d.force_raycast_update()
+	
+	if ray_cast_2d.is_colliding():
+		return
+	
+	is_moving = true
+	
+	global_position = tile_map.map_to_local(target_tile)
+	
+	sprite_2d.global_position = tile_map.map_to_local(current_tile)
