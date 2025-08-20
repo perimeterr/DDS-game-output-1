cbbf5b639c509fcdf0fef2fb3522fea4d1837287
perimeterr <mikaela.paderna@student.ateneo.edu>
2025-08-20 14:08:53 +0800

    implemented player Idle and Walk animations into movement

--- player.gd
+++ player.gd
@@ -1,9 +1,14 @@
 extends Node2D
 
-@onready var tile_map = $"../TileMap"
-@onready var sprite_2d = $Sprite2D
-@onready var ray_cast_2d = $RayCast2D
+@onready var tile_map : TileMap = $"../TileMap"
+@onready var sprite_2d : Sprite2D = $Sprite2D
+@onready var ray_cast_2d : RayCast2D = $RayCast2D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
 var is_moving = false
+var cardinal_direction : Vector2 = Vector2.DOWN
+var direction : Vector2 = Vector2.ZERO
+var state : String = "idle"
 
 
 func _physics_process(delta):
@@ -17,19 +22,29 @@ func _physics_process(delta):
 	sprite_2d.global_position = sprite_2d.global_position.move_toward(global_position, 1)
 
 func _process(delta):
+	if SetState() || SetDirection():
+		UpdateAnimation()
+	
 	if is_moving:
 		return
 		
 	if Input.is_action_pressed("up"):
-		move(Vector2.UP)
+		direction = Vector2.UP
 	elif Input.is_action_pressed("down"):
-		move(Vector2.DOWN)
+		direction = Vector2.DOWN
 	elif Input.is_action_pressed("left"):
-		move(Vector2.LEFT)
+		direction = Vector2.LEFT
 	elif Input.is_action_pressed("right"):
-		move(Vector2.RIGHT)
+		direction = Vector2.RIGHT
+	else:
+		direction = Vector2.ZERO
+		
+	move(direction)
 
 func move(direction: Vector2):
+	if direction == Vector2.ZERO:
+		return
+	
 	var current_tile: Vector2i = tile_map.local_to_map(global_position)
 	
 	var target_tile: Vector2i = Vector2i(
@@ -53,3 +68,36 @@ func move(direction: Vector2):
 	global_position = tile_map.map_to_local(target_tile)
 	
 	sprite_2d.global_position = tile_map.map_to_local(current_tile)
+	
+func SetDirection() -> bool:
+	var new_dir : Vector2 = direction
+	if new_dir == Vector2.ZERO:
+		return false
+		
+	if new_dir == cardinal_direction:
+		return false
+		
+	cardinal_direction = new_dir
+	
+	return true
+	
+func SetState() -> bool:
+	var new_state : String = "idle" if direction == Vector2.ZERO else "walk"
+	if new_state == state:
+		return false
+	state = new_state
+	return true
+	
+func UpdateAnimation() -> void:
+	animation_player.play( state + "_" + AnimDirection())
+	pass
+	
+func AnimDirection() -> String:
+	if cardinal_direction == Vector2.DOWN:
+		return "down"
+	elif cardinal_direction == Vector2.UP:
+		return "up"	
+	elif cardinal_direction == Vector2.LEFT:
+		return "left"
+	else:
+		return "right"	
